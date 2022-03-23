ff4365f753390b3f62103a75ca8925b31b9ce671
Nicholas2750 <nicholashusin@gmail.com>
2022-03-23 16:09:14 -0500

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content : [
+    './_includes/**/*.html',
+    './_layouts/**/*.html',
+    './_posts/*.md',
+    './*.html',
+
+  ],
+  theme : {
+    extend : {},
+
+  },
+  variants : {},
+  plugins : [
+    require('@tailwindcss/typography'),
+
+  ],
+
+}
